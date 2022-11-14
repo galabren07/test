2cb6ccd467e9fb59987e0bc0a842e1ce70cb9be4
galabren07 <galabren07@outlook.com>
2022-11-13 23:09:38 -0700

    a message

--- Promineo_Tech/Week-06-DevTools_Debugging_and_Unit_Tests/WarCardGame.js
+++ Promineo_Tech/Week-06-DevTools_Debugging_and_Unit_Tests/WarCardGame.js
@@ -1,174 +1,193 @@
-console.clear();
-var Card = (function() {
-    var suitNames = ['spades', 'diamonds', 'clubs', 'hearts'];
-    suitCodes = ['\u2660', '\u2666', '\u2663', '\u2665'],
+console.log("this is my WarCardGame");
+
+const suits = ["♣", "♣", "♥", "♠"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+var playerOne = [];
+var playerTwo = [];
+
+
+
+ class Deck {
+    constructor(cards = newDeck()) {
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length;
+    }
+
+    pop() {
+        return this.cards.shift()
+    }
+
+    push(card) {
+        this.cards.push(card)
+    }
+
+    const shuffleIt = () => {
+        shuffleIt = () => {
+             this.deck.sort(() => 0.5 - Math.random());
+        }
+      
+       }
+ }
+ //shuffle cards to be random using this for loop
+shuffleIt();
+       for (let i = this.numberOfCards.length - 1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * (i +1));
+        function shuffleIt() {
+            for (let i = this.numberOfCards.length - 1; i > 0; i--) {
+                const newIndex = Math.floor(Math.random () * (i + 1));
+                 const oldValue = this.cards[newIndex]
+                  this.cards[newIndex] = this.cards[i];
+                        this.cards[i] = oldValue
+        }
+      
+    }
+       }
+
+       let newDeck = new Deck()
+       console.log("The deck has been shuffled and is ready to deal to play");
+       console.log(newDeck.deck);
+       
+
+       this.rank = rank
+
+
+
+
+
+    class Card {
+    constructor(value, suit, rank) {
+this.value = value
+this.suit = suit
+this.rank = rank
     
-    Card = function(index) {
-        this.index = index;
-        this.value = (index % 13)+1;
-        this.suit = suitNames[Math.floor(index/13)];
- };
 
- Card.prototype = {
-    get number() {
-        switch(this.value) {
-case 11:
-    return 'J';
-case 12:
-    return 'Q';
-case 13:
-    return 'K';
-    case 1:
-        return 'A';
-        default:
-            return this.value;
-        }
-        
-
-    },
-    get name() {
-        var numberName = (function(n){
-            switch(n) {
-                case 'A': return 'Ace';
-                case 'K': return 'King';
-                case 'Q': return 'Queen';
-                case 'J': return 'Jack';
-                default: return n;
-            }
-})(this.number);
-return numberName + ' of ' + this.suit;
-    },
-    get suitUnicodeArray() {return suitCode; 
-    },
-        get suitNameArray() { return suitNames; }
-        },
-        returnCard;
-    })();
-    console.log(new Card(13));
-
-    /*** START GAME SETUP ***/
-
-    // Create Deck of cards
-
-    var deck = Array.apply(null,Array(52)).map(function(_, i){return new Card(i);});
-
-    //Create player decks
-     var playerDeck = [], cpuDeck = [],
-     drawIndex;
-
-     //Deal cards to players (2)
-     while( deck.length > 0 ){
-
-        // Draw Card for Player
-        drawIndex = Math.random() * deck.length;
-        playerDeck.push(deck.splice(drawIndex, 1)[0] );
-
-        // Draw Card for CPU
-        drawIndex = Math.random() * deck.length; cpuDeck.push(deck.splice(drawIndex.cpu, 1[0]));
-     }
-
-
- /*** END GAME SETUP ***/
   
- //Single Round
- var drawAndPlay = function(rewards){
-    if( rewards ){
-        console.log('rewards = ', rewards); }
-        
-        // if either deck is empty, game over
-        if( playerDeck.length === 0 || cpuDeck.length === 0 ){
-//game over
-if( playerDeck.length > 0 ){
-    console.log('Player Won');
-}
-return false;
+        this.suits = ["♣", "♣", "♥", "♠"];
+        this.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        this.ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+    
+   }
+
+    createDeck() {
+        for(let suit of this.suits) {
+            for(let value of values) {
+                this.deck.push(newCard(value, suit));
+            }
         }
-        // draw card from each deck
-        var playerCard = playerDeck.shift(),
-        cpuCard = cpuDeck.shift(),
-        rewards = rewards ? rewards: [];
-
-        // Update View for Drawn Cards
-        TODO: Optimize
-        var PlayerSection = document.querySelection('section.player'),
-        cpuSection = playerSection.querySelection('div.card');
-        playerCardDiv = cpuSection.querySelection('div.card'),
-        cpuCardDiv = cpuSection.querySelection('div.card');
-        playerPoints = playerSection.querySelection('[data-points'),
-        cpuPoints = cpuSection.querySelection('[data-points');
-
-        Card.prototype.suitNameArray.forEach(function(v,i,a){
-
-            playerCardDiv.classList.remove(v);
-
-            cpuCardDiv.classList.remove(v);
-            return true;
+    }
+} 
+    function newDeck() {
+        return suits.flatMap(suit => {
+            return values.map(value => {
+                return new Card(suit, value)
+            });
         });
 
-        playerCardDiv.querySelection('span.name').innerHTML = playerCard.number;
-        playerCardDiv.classList.add( playerCard.suit );
+    }
+class Player {
+constructor(playerOne, playerTwo, points) {
+    this.playerOne = playerOne
+    this.playerTwo = playerTwo
+    this.points = points;
+   
 
-        playerSection.querySelection('div[date-cards-left]').setAttribute('data-cards-left', playerDeck.length);
-
-        cpuCardDiv.querySelection('span.name').innerHTML = cpuCard.number;
-        cpuCardDiv.classList.add( cpuCard.suit );
-
-        cpuSection.querySelection('div[data-cards-left]').setAttribute('data-cards-left', cpuDeck.length);
-
-        // compare cards
-        if( playerCard.value === cpuCard.value ){
-            console.log('tie', playerCard, cpuCard);
-
-            rewards.push(playerCard);
-            rewards.push(cpuCard);
-            return drawAndPlay(rewards);
-
-        }else if( playerCard.value > cpuCard.value ){
-            //Player wins
-            console.log('Player wins round', playerCard, cpuCard);
-            // Add point to player score
-            TODO
-
-            playerPoints.setAttribute('data-points',parseInt(playerPoints.getAttribute('data-points'))+1)
-
-            //Reward Cards
-            playerDeck.splice(playerDeck.length, 0, playerCard, cpuCard);
-            if( rewards.length > 0 ){
-                playerDeck.concat(rewards);
-            }
-
-        } else {
-            //CPU Wins
-            console.log('CPU wins round', playerCard, cpuCard);
-            // Add point to CPU score
-            TODO
-
-            cpuPoints.setAttribute('data-points', parseInt(cpuPoints.getAttribute('data-points'))+1)
-
-            //Reward Cards
-
-            cpuDeck.splice(cpuDeck.length, 0, cpuCard, playerCard);
-            if( rewards.length > 0 ){
-                cpuDeck = cpuDeck.concat(rewards);
-            }
-        }
-console.log('Player Cards left = '+playerDeck.length, 'CPU Cards left = '+cpuDeck.length);
-return true;
+    var x = deal(playerOne, playerTwo);
+    expect(playerOne.length).to.equal(26);
+    expect(playerTwo.length).to.equal(26);
+    console.log("Both players should contain 26 elements");
+}
 
 
+}
+const dealCards = (arr) => {
+    let half = Math.ceil(arr.length / 2);
+    playerOne = arr.slice(0, half);
+    playerTwo = arr.slice(half);
+}
 
- };
- window.onload = function(){
-    var btn = document.querySelector('button.draw');
-    btn.addEventListener('click', function(event){
-        var keepGoing = drawAndPlay();
-        if( !keepGoing ){
-            alert('Game Over');
-            
-        }
-    });
+//let myNewDeck = new Deck ();
+//myNewDeck.createDeck ();
+//console.log(myNewDeck);
 
+startGame
+function startGame() {
+    const deck = new Deck()
+    deck.shuffleCards()
+    
+    const split = (deck.numberOfCards / 2)
+    playerOne = Deck(deck.cards.slice(0, split))
+    playerTwo = Deck(deck.cards.slice(split, deck.numberOfCards))
+
+    console.log(playerOne)
+        console.log(playerTwo)
+    }
+    
+    const deck = new Deck();
+    console.log(deck.cards);
+    var playerOneCardValue = playerOne.cards[0].value
+    var playerTwoCardValue = playerTwo.cards[0].value
+    let playerOneCardValue = playerOne.cards
+    let playerTwoCardValue = playerTwo.cards
+
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    
+
+startGame()
+function startGame() {
+    const deck = new Deck()
+    deck.shuffle()
+
+    const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.lastIndexOf(0, deckMidpoint))
+    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+    inRound = false
+    stop = false
+
+updateDeckCount()
+
+    if (isRoundWinner(playerOne, PlayerTwo)) {
+        Text.innerText = "Win"
+        playerDeck.push(playerOne)
+        playerDeck.push(playerTwo)
+
+    }else if (isRoundWinner(playerTwo, playerOne))
+    text.innerText = "Lose"
+    computerDeck.push(playerOne)
+    computerDeck.push(playerTwo)
+} if (isRoundWinner(playerTwo, playerOne)) {
+    text.innerText = "Lose"
+    computerDeck.push(playerOne)
+    computerDeck.push(playerTwo)
+} else {
+    text.inner.Text = "Draw"
+    playerDeck.push(playerOne)
+    computerDeck.push(playerTwo)
+}
+ if (isGameOver(playerDeck)) {
+    console.log("You Lose!!")
+    stop = true
+ } else if (isGameOver(computerDeck)) {
+    console.log("You Win!!")
+    stop = true
  }
+ console.log(this.playerOne +" loses war");
+ 
+
+
+
+
+
+
+
+
   
 
- 
+
+
+
+    
