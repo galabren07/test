e9c62ae9178331f49c67a2ffcb3a6299d1ef9ec4
galabren07 <galabren07@outlook.com>
2022-11-14 20:10:04 -0700

    a message

--- Promineo_Tech/Week-06-DevTools_Debugging_and_Unit_Tests/WarCardGame.js
+++ Promineo_Tech/Week-06-DevTools_Debugging_and_Unit_Tests/WarCardGame.js
@@ -1,198 +1,190 @@
 console.log("this is my WarCardGame");
 
-const suits = ["♣", "♦", "♥", "♠"];
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+const suits = ["♣", "♦" , "♥", "♠"];
+const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 var playerOne = [];
 var playerTwo = [];
 
 
 
- class Deck {
-    constructor(cards = newDeck()) {
-        this.cards = cards
-    }
+//  class Deck {
+//     constructor(cards = newDeck()) {
+//         this.cards = cards
+//     }
 
-    get numberOfCards() {
-        return this.cards.length;
-    }
+//     get numberOfCards() {
+//         return this.cards.length;
+//     }
 
-    pop() {
-        return this.cards.shift()
-    }
+//     pop() {
+//         return this.cards.shift()
+//     }
 
-    push(card) {
-        this.cards.push(card)
+//     push(card) {
+//         this.cards.push(card)
 
-    }
- shuffleIt = () => {
-             this.deck.sort(() => 0.5 - Math.random());
-        }
+//     }
+//  shuffleIt = () => {
+//              this.deck.sort(() => 0.5 - Math.random());
+//         }
       
-       }
+//        }
  
- //shuffle cards to be random using this for loop
-shuffleIt();
-      function shuffleIt() {
-            for (let i = this.numberOfCards.length - 1; i > 0; i--) {
-                const newIndex = Math.floor(Math.random () * (i + 1));
-                 const oldValue = this.cards[newIndex]
-                  this.cards[newIndex] = this.cards[i];
-                        this.cards[i] = oldValue
-        }
+//  //shuffle cards to be random using this for loop
+// // shuffleIt();
+// //       function shuffleIt() {
+// //             for (let i = this.numberOfCards.length - 1; i > 0; i--) {
+// //                 const newIndex = Math.floor(Math.random () * (i + 1));
+// //                  const oldValue = this.cards[newIndex]
+// //                   this.cards[newIndex] = this.cards[i];
+// //                         this.cards[i] = oldValue
+// //         }
       
-    }
+// //     }
        
 
 
 
-function newDeck() {
-    function newDeck() {
-        return suits.flatMap(suit => {
-                return values.map(value => {
-                return new Card(suit, value)
-    })
-        })
+// function newDeck() {
+//     function newDeck() {
+//         return suits.flatMap(suit => {
+//                 return values.map(value => {
+//                 return new Card(suit, value)
+//     })
+//         })
             
-    }
+//     }
 
-    }
+//     }
 
-    class Card {
-        constructor(value, suit, rank) {
-            this.value = value
-            this.suit = suit
-            this.rank = rank
-              
-
-            this.suit = ["♣", "♦", "♥", "♠"];
-            this.values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-            this.ranks =  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
-            this.deck = [];
+//     let myDeck = new Deck()
+//     console.log(myDeck)
+   
+class Deck {
+    constructor(suits, ranks) {
+        this.suit = suits;
+        this.rank = ranks;
+        this.deck = [];
         }
         createDeck() {
-            for(let suit of this.suits) {
-                for(let value of values){
-this.deck.push(newCard(value, suit));
+            for (let i = 0; i < suits.length; i++) {
+                for (let x = 0; x < ranks.length; x++) {
+                          this.deck.push({
+                            value: x +1,
+                            suit: suits[i],
+                            rank: ranks[x],
+
+                        });
+
+                    
                 }
             }
-        
         }
-    }
-     
-    function newDeck(){
-        return suits.flatMap(suit => {
-            return values.map(value => {
-return new Card(suit, value)
-            })
-        })
-    }
+    shuffleIt () {
+    this.deck.sort(() => 0.5 - Math.random());
+
+}
+}   
+//     function newDeck(){
+//         return suits.flatMap(suit => {
+//             return values.map(value => {
+// return new Card(suit, value)
+//             })
+//         })
+//     }
     
-    const freshDeck = new Deck();
-    console.log("The deck has been shuffled and is ready to deal to play")
+
+    
+const freshDeck = new Deck();
+freshDeck.createDeck();
+freshDeck.shuffleIt();
+    console.log('Deck is shuffled and ready to play')
     console.log(freshDeck.deck);
 
 
+
 class Player {
-    constructor(playerOne, playerTwo, points) {
-this.playerOne = playerOne
-this.playerTwo = playerTwo
-this.points = points;
-
-var x = deal(playerOne, playerTwo);
-expect(playerOne.length).to.equal(26);
-expect(playerTwo.length).to.equal(26);
-console.log("Both players should contain 26 elements");
-    }
-
+    constructor(name) {
+this.playerName = name
+this.points = 0;
 }
- let myNewDeck = new Deck ();
- console.log(myNewDeck);
+}
+let playerA = new Player ('Tom')
+console.log(playerA)
 
- startGame
- function startGame() {
-    const deck = new Deck()
-    deck.shuffleCards()
-
-    const split = (deck.numberOfCards / 2)
-    playerOne = Deck(deck.cards.slice(0, split))
-    playerTwo = Deck(deck.cards.slice(split, deck.numberOfCards))
-    console.log(playerOne)
-    console.log(playerTwo)
- }
-
-  deck = Deck();
+let playerB = new Player ( 'Sam')
+console.log(playerB)
+// var x = deal(playerOne, playerTwo);
+// expect(playerOne.length).to.equal(26);
+// expect(playerTwo.length).to.equal(26);
+// console.log("Both players should contain 26 elements");
+//     }
 
 
-const dealCards = (arr) => {
+//     const split = (deck.numberOfCards / 2)
+//     playerOne = Deck(deck.cards.slice(0, split))
+//     playerTwo = Deck(deck.cards.slice(split, deck.numberOfCards))
+//     console.log(playerOne)
+//     console.log(playerTwo)
+//  }
+
+  //deck = Deck();
+let playerHandA = [];
+let playerHandB = [];
+
+  dealCards = (arr) => {
     let half = Math.ceil(arr.length / 2);
-    playerOne = arr.slice(0, half);
-    playerTwo = arr.slice(half);
+    playerHandA = arr.slice(0, half);
+    playerHandB = arr.slice(half);
 
 }
+dealCards (freshDeck.deck);
+console.log('player Tom: ')
+console.log(playerHandA)
+console.log(playerB.playerName)
+console.log(playerHandB)
 //let myNewDeck = new Deck ();
 //console.log(myNewDeck);
 
-startGame
-function startGame() {
-    const deck = new Deck()
-    deck.shuffleCards()
-    
-    const split = (deck.numberOfCards / 2)
-    playerOne = Deck(deck.cards.slice(0, split))
-    playerTwo = Deck(deck.cards.slice(split, deck.numberOfCards))
+class Game {
+   startGame () {
+for (let i = 0; i < 26; i++) {
+    console.log(playerA.playerName + " 's card: " + playerHandA[i].rank + ' of ' + playerHandA[i].suit)
+    console.log(playerB.playerName + " 's card: " + playerHandB[i].rank + ' of ' + playerHandB[i].suit)
+if (playerHandA[i].value > playerHandB[i].value) {
+    playerA.points += 1;
+    console.log(playerA)
+    console.log(playerB)
+} else if (playerHandA[i].value < playerHandB[i].value){
+    playerB.points += 1;
+    console.log(playerA)
+    console.log(playerB)
+}else{
+    console.log('tie')
+}
+console.log(`${playerA.playerName}'s final score: ${playerA.points}`)
+console.log(`${playerB.playerName}'s final score: ${playerB.points}`)
 
-    console.log(playerOne)
-        console.log(playerTwo)
+
+}
+}
+}
+
+    let playGame = new Game();
+    playGame.startGame();
+    
+    let theWinner = () => {
+        if (playerA.points > playerB.points) {
+            console.log(`${playerA.playerName} Wins!`)
+     }else if (playerB.points > playerA.points) {
+        console.log(`${playerB.playerName} Wins!`)
+        }else {
+            console.log('Tie Game')
+        }
     }
-    
-    const deck = new Deck();
-    console.log(deck.cards);
-    let playerOneCardValue = playerOne.cards
-    let playerTwoCardValue = playerTwo.cards
 
-    playerOneScore = 0;
-    playerTwoScore = 0;
-    
-
-startGame()
-function startGame() {
-    const deck = new Deck()
-
-    const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-    playerDeck = new Deck(deck.cards.lastIndexOf(0, deckMidpoint))
-    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
-    inRound = false
-    stop = false
-}
-function updateDeckCount () {
-
-    if (isRoundWinner(playerOne, playerTwo)) {
-        Text.innerText = "Win"
-        playerDeck.push(playerOne)
-        playerDeck.push(playerTwo)
- }else if (isRoundWinner(playerTwo, playerOne))
-    text.innerText = "Lose"
-    computerDeck.push(playerOne)
-    computerDeck.push(playerTwo)
-} if (isRoundWinner(playerTwo, playerOne)) {
-    text.innerText = "Lose"
-    computerDeck.push(playerOne)
-    computerDeck.push(playerTwo)
-
-} else {
-    text.inner.Text = "Draw"
-    playerDeck.push(playerOne)
-    computerDeck.push(playerTwo)
-}
-if (isGameOver(playerDeck)) {
-    console.log("You Lose!!")
-    stop = true
- } else if (isGameOver(computerDeck)) {
-    console.log("You Win!!")
-    stop = true
- }
-
+    theWinner();
  
 
 
